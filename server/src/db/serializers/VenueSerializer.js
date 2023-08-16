0c784b61bc9d1d9d1ccc4d545fd86afc0d3ed837
data-nihilist <mattmccredy@gmail.com>
2023-08-16 17:17:22 -0400

    initial serializers for venues and shows

---
+++ server/src/db/serializers/VenueSerializer.js
@@ -0,0 +1,46 @@
+import ShowSerializer from "./ShowSerializer.js"
+
+class VenueSerializer {
+    static async summarize(array) {
+        const serializedVenues = array.map((venue) => {
+            const requiredAttributes = [
+                "id",
+                "name",
+                "location",
+                "hostId"
+            ];
+    
+            let serializedVenue = {};
+            for (const attribute of requiredAttributes) {
+                serializedVenue[attribute] = venue[attribute]
+            }
+
+            return serializedVenue;
+        })
+
+        return serializedVenues
+    }
+
+    static async withRelated(venue) {
+
+            const requiredAttributes = [
+                "id",
+                "name",
+                "location",
+                "hostId"
+            ];
+    
+            let serializedVenue = {};
+            for (const attribute of requiredAttributes) {
+                serializedVenue[attribute] = venue[attribute]
+            }
+
+            const shows = await venue.$relatedQuery("shows")
+            serializedVenue.shows = await ShowSerializer.summarize(shows)
+
+            return serializedVenue;
+
+    }
+}
+
+export default VenueSerializer
