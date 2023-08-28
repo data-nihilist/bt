d5e74ce3bbf634bece66ac2cd1583c0ad19bf159
data-nihilist <mattmccredy@gmail.com>
2023-08-28 14:22:24 -0400

    updating spotify key for heroku app

--- client/src/components/spotify/useAuth.js
+++ client/src/components/spotify/useAuth.js
@@ -8,7 +8,7 @@ export default function useAuth(code) {
 
     useEffect(() => {
         axios
-            .post('http://localhost:3000/showplayground', {
+            .post('https://get-to-the-gig-9923dd0273ee.herokuapp.com/showplayground', {
                 code,
             })
             .then(res => {
@@ -25,7 +25,7 @@ export default function useAuth(code) {
         if (!refreshToken || !expiresIn) return
         const interval = setInterval(() => {
             axios
-                .post('http://localhost:3000/refresh', {
+                .post('https://get-to-the-gig-9923dd0273ee.herokuapp.com/refresh', {
                     refreshToken,
                 })
                 .then(res => {
